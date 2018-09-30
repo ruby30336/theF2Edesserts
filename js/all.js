fe914153b1e65c841fce10bf391ba87a2fb2cada
ruby30336 <ruby30336@gmail.com>
2018-09-30 18:54:48 +0800

    20180930

--- js/all.js
+++ js/all.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-	// 首頁愛心
+	// 點擊喜歡
 	$('.far.fa-heart').click(function(event) {
 		/* Act on the event */
 		$(this).toggleClass("click");
+		$(this).toggleClass("animated bounceIn");
 	});
-	// 產品頁左側菜單
-
+	// 點擊垃圾桶
+	$('.your-cart ul li a').click(function(event) {
+		/* Act on the event */
+		$(this).parent().parent().parent().remove();
+	});
+	// 發票頁按鈕
+	$('.choise-receipt button').click(function(event) {
+		/* Act on the event */
+		$(this).toggleClass("click");
+		$(this).siblings().toggleClass("click");
+	});
+	// 發票頁切換
+	$('.btn-ele-receipt').click(function(event) {
+		/* Act on the event */
+		$('.ele-receipt').toggleClass("click");
+		$('.mail-receipt').toggleClass("click");
+	});
+	$('.btn-mail-receipt').click(function(event) {
+		/* Act on the event */
+		$('.ele-receipt').toggleClass("click");
+		$('.mail-receipt').toggleClass("click");
+	});
 });
