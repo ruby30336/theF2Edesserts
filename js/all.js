2212b8400155efbaaf85211dfcfb4efa1ce86d1a
ruby30336 <43534032+ruby30336@users.noreply.github.com>
2019-01-19 13:03:16 +0800

    Add files via upload
    add BS4 and RWD.

--- js/all.js
+++ js/all.js
@@ -1,40 +1,18 @@
-$(document).ready(function() {
-	// 點擊喜歡
-	$('.far.fa-heart').click(function(event) {
-		/* Act on the event */
-		$(this).toggleClass("click");
-		$(this).toggleClass("animated bounceIn");
-	});
-	// 點擊垃圾桶
-	$('.your-cart ul li a').click(function(event) {
-		/* Act on the event */
-		$(this).parent().parent().remove();
-	});
-	// 發票頁按鈕
-	$('.choise-receipt button').click(function(event) {
-		/* Act on the event */
-		$(this).toggleClass("click");
-		$(this).siblings().toggleClass("click");
-	});
-	// 發票頁切換
-	$('.btn-ele-receipt').click(function(event) {
-		/* Act on the event */
-		$('.ele-receipt').toggleClass("click");
-		$('.mail-receipt').toggleClass("click");
-	});
-	$('.btn-mail-receipt').click(function(event) {
-		/* Act on the event */
-		$('.ele-receipt').toggleClass("click");
-		$('.mail-receipt').toggleClass("click");
-	});
-	// 手機版漢堡選單
-	$('.drop-down').click(function(event) {
-		/* Act on the event */
-		$('.top-menu-mb ul').slideToggle(500);
-	});
-	// 手機漢堡選單按鈕動畫
-	$('.drop-down').click(function(event) {
-		/* Act on the event */
-		$('.drop-down').toggleClass("click");
-	});
+$(document).ready(function () {
+    // favorite-animation
+    $('.likeit').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('animated bounceIn');
+        $(this).toggleClass('active');
+        
+    });
+    // checkout_3.html-receiptButtonSwitch
+    $('.receipt button').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('#electronic-receipt').toggleClass('d-none');
+        $(this).siblings().toggleClass('active');
+        $('#mail-receipt').toggleClass('d-none');
+        
+    });
 });
